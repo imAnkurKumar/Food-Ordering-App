4560f91a5897129d0bb8a14c080fa3562476f395
Ankur Kumar <imankur.ak@gmail.com>
2026-03-22 06:46:09 +0530

    Routing

--- App.js
+++ App.js
@@ -2,15 +2,42 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import About from "./src/components/About";
+import ContactUs from "./src/components/ContactUs";
+import Error from "./src/components/Error";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
